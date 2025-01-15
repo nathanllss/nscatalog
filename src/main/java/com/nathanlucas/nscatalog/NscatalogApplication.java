cfb82f5325e36c180dc8f077eb8241b103caa787
Nathan <nathan.llucasl@gmail.com>
2025-01-15 09:34:11 -0300

    initializing repo

---
+++ src/main/java/com/nathanlucas/nscatalog/NscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.nathanlucas.nscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NscatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathanlucas/nscatalog/NscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathanlucas.nscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
